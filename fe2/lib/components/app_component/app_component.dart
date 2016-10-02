71f6588313cb9de10cecaf33b1d3010f3409fa8b
Matt L <leiboldm@umich.edu>
2016-10-02 15:35:52 -0400

    Revert "Github deleted everything for some reason."
    This reverts commit f44019d08e920d3affe94a81da7aef2fb702c85a.

--- fe2/lib/components/app_component/app_component.dart
+++ fe2/lib/components/app_component/app_component.dart
@@ -3,6 +3,8 @@ import 'package:angular2/router.dart';
 import 'package:fe2/components/login/login_component.dart';
 import 'package:fe2/components/create_user/create_user_component.dart';
 import 'package:fe2/components/lookup/lookup_component.dart';
+import 'package:fe2/service/service.dart';
+import 'package:fe2/utils/app_state.dart';
 import 'dart:html';
 import 'dart:convert';
 
@@ -16,16 +18,26 @@ import 'dart:convert';
   const Route(path: 'lookup', name: 'Lookup', component: LookupComponent)
 ])
 class AppComponent {
-  String backendRoot = 'http://mattleibold.com/SpanCard/Backend/api/';
   Router _router;
-  AppComponent(this._router) {
-    HttpRequest.getString(backendRoot + 'login.php').then((String data) {
+  AppState _appState;
+  bool get loggedIn => _appState.isLoggedIn;
+
+  AppComponent(this._router, this._appState) {
+    sendGetRequest('login.php').then((String data) {
       var resp = JSON.decode(data);
-      if (resp['isLoggedIn'] == true) {
+      _appState.isLoggedIn = resp['loggedIn'];
+      if (resp['loggedIn'] == true) {
         _router.navigate(['Lookup']);
       } else {
         _router.navigate(['Login']);
       }
+    });
+  }
+
+  logout() {
+    sendGetRequest('logout.php').then((String data) {
+      _appState.isLoggedIn = false;
+      _router.navigate(['Login']);
     });
   }
 }
--- fe2/lib/components/lookup/lookup_component.dart
+++ fe2/lib/components/lookup/lookup_component.dart
@@ -1,6 +1,28 @@
 import 'package:angular2/core.dart';
+import 'package:fe2/service/service.dart';
+import 'package:angular2/router.dart';
+import 'dart:convert';
 
 @Component(
     selector: 'lookup',
     templateUrl: 'lookup.html')
-class LookupComponent {}
+class LookupComponent {
+  String word;
+  List translations;
+  String toWord;
+  Router _router;
+
+  LookupComponent(this._router);
+
+  lookupWord() {
+    print('lookup word $word');
+  }
+
+  addToFlashCardSet() {
+    print('add to flash card set');
+  }
+
+  startPractice() {
+    print('start practice');
+  }
+}
