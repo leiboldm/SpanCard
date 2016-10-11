7ad6c7798bd884038162e2255c638dcb65dcc081
Matt L <leiboldm@umich.edu>
2016-10-11 02:47:59 -0400

    Implement create account in angular frontend

--- fe2/lib/components/app_component/app_component.dart
+++ fe2/lib/components/app_component/app_component.dart
@@ -1,7 +1,6 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 import 'package:fe2/components/login/login_component.dart';
-import 'package:fe2/components/create_user/create_user_component.dart';
 import 'package:fe2/components/lookup/lookup_component.dart';
 import 'package:fe2/components/practice/practice_component.dart';
 import 'package:fe2/service/service.dart';
@@ -15,7 +14,6 @@ import 'dart:convert';
     directives: const [ROUTER_DIRECTIVES])
 @RouteConfig(const [
   const Route(path: 'login', name: 'Login', component: LoginComponent),
-  const Route(path: 'create_user', name: 'CreateUser', component: CreateUserComponent),
   const Route(path: 'lookup', name: 'Lookup', component: LookupComponent, useAsDefault: true),
   const Route(path: 'practice', name: 'Practice', component: FlashcardPracticeComponent)
 ])
--- fe2/lib/components/create_user/create_user_component.dart
+++
@@ -1,8 +0,0 @@
-import 'package:angular2/core.dart';
-import 'package:fe2/components/header/header.dart';
-
-@Component(
-    selector: 'create-user',
-    templateUrl: 'create_user.html',
-    directives: const [HeaderComponent])
-class CreateUserComponent {}
